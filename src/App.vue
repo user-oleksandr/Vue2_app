df8fc8c003a63bf7896739d94705db2596273d79
Oleksandr <name.oleksandr@gmail.com>
2023-08-13 22:52:22 +0300

    create app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="userInput" placeholder="Введіть номер будинку" />
+    <button @click="checkNumber">Перевірити</button>
+
+    <building-validator
+        v-for="(item, index) in paginatedBuildings"
+        :key="index"
+        :building-number="item"
+    ></building-validator>
+
+    <div>
+      <button @click="prevPage" :disabled="currentPage === 1">Попередня сторінка</button>
+      <button @click="nextPage" :disabled="currentPage === totalPages">Наступна сторінка</button>
+      <span>Сторінка {{ currentPage }} з {{ totalPages }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BuildingValidator from "@/components/BuildingValidator";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    BuildingValidator,
+  },
+  data() {
+    return {
+      buildingNumbers: [],
+      userInput: '',
+      itemsPerPage: 1000,
+      currentPage: 1,
+    };
+  },
+  computed: {
+    paginatedBuildings() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.buildingNumbers.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.buildingNumbers.length / this.itemsPerPage);
+    },
+  },
+  methods: {
+    checkNumber() {
+      const isValid = /^[0-9/-]+$/.test(this.userInput);
+      alert(isValid ? 'Номер валідний' : 'Номер невалідний');
+    },
+    async loadBuildingNumbers() {
+      try {
+        const response = await fetch('/buildingNumbers.txt');
+        const text = await response.text();
+        this.buildingNumbers = text.split('\n').filter(Boolean);
+      } catch (error) {
+        console.error('Помилка завантаження даних:', error);
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage -= 1;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage += 1;
+      }
+    },
+  },
+  mounted() {
+    this.loadBuildingNumbers();
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 20px;
 }
 </style>
---
+++ src/components/BuildingValidator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span :style="{ color: isValidBuilding ? 'green' : 'red' }">
+      {{ buildingNumber }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buildingNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    isValidBuilding() {
+      return /^[0-9/-]+$/.test(this.buildingNumber);
+    },
+  },
+};
+</script>
